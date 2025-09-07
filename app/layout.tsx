f640831a8c614ab98f378401f03a396f929d1f17
mojahidomer <38102741+mojahidomer@users.noreply.github.com>
2025-09-07 22:51:53 +0530

    feat: update logo

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
         <title>Dr Lal PathLabs - Leading Diagnostic Laboratory</title>
         <meta name="description" content="Trusted diagnostic services with advanced testing facilities. Book your health checkups online." />
         <link rel="icon" href="/logo.png" sizes="any" />
-        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+        <link rel="apple-touch-icon" href="/lal-3.png" />
       </head>
       <body className={inter.className}>
       <Providers >
